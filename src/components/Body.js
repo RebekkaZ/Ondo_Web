0a58812a63872ff73f1478701a6c6a7dc47cd963
RebekkaZ <rebekka.zahler@outlook.com>
2019-07-02 12:44:19 +0800

    add tab state coloring

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,20 +4,23 @@ import './Body.css'
 import 'react-tabs/style/react-tabs.css';
 import AirConItem from './AirConItem';
 import aircondata from '../data.json';
-
+import { bindActionCreators } from 'redux';
+import * as ondoActions from '../redux/actions/actions'
+import { connect } from 'react-redux'
 
 class Body extends Component {
     constructor(props) {
         super(props);
         this.aircons = aircondata.airCons;
-        this.state = {};
     }
 
-
     render() {
-
-        let tabsList = this.aircons.map((e, i) =>
-            <Tab key={i}>{e.name}</Tab>
+        let tabsList = this.aircons.map((e, i) => {
+            return <Tab
+                style={this.props.acSettings.find(acSetting => acSetting.id === e.id) === undefined || this.props.acSettings.find(acSetting => acSetting.id === e.id).devicePower === undefined ? { backgroundColor: '#E8E8E8' } : this.props.acSettings.find(acSetting => acSetting.id === e.id).devicePower === true ? { backgroundColor: '#E0F4CF' } : { backgroundColor: '#F4CFCF' }}
+                key={i}>{e.name}
+            </Tab>
+        }
         );
 
         let tabsContent = this.aircons.map((e, i) =>
@@ -39,7 +42,26 @@ class Body extends Component {
             </div>
         )
     }
+
+    async componentDidMount() {
+        this.aircons.map((e, i) =>
+            this.props.actions.loadAcSetting(e.id).catch(error => {
+                console.log(error);
+            }))
+
+    }
 }
 
+function mapStateToProps(state) {
+    return {
+        acSettings: state.acSettings
+    }
+}
 
-export default Body;
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(ondoActions, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Body);
